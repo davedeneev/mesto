bd3f5bae843b852ea7765fbdb8b70a36657ecfc7
davedeneev <davedeneev@yandex.ru>
2022-12-22 16:02:23 +0300

    Вторая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,25 @@
 const setPopup = document.querySelector('.popup');
 const btnProfileEdit = document.querySelector('.profile__edit-btn');
-const btnExitProfile = document.querySelector('.popup__profile-exit-btn');
-const btnSaveProfile = document.querySelector('.popup__profile-form');
+const btnExitProfile = document.querySelector('.popup__exit-btn');
+const formProfile = document.querySelector('.popup__form');
 const setName = document.querySelector('.profile__name');
 const setDesc = document.querySelector('.profile__description');
-const inputName = document.querySelector('.popup__profile-name');
-const inputDesc = document.querySelector('.popup__profile-desc');
-const btnLike = document.querySelectorAll('.place-card__like-btn');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputDesc = document.querySelector('.popup__input_type_desc');
 
-btnProfileEdit.addEventListener('click', function () {
+function showProfilePopup() {
     inputName.value = setName.textContent;
     inputDesc.value = setDesc.textContent;
-    setPopup.classList.add('popup_opened');
-});
+    setPopup.classList.toggle('popup_opened');
+}
 
-btnExitProfile.addEventListener('click', function () {
-    setPopup.classList.remove('popup_opened');
-});
-
-btnSaveProfile.addEventListener('submit', function (evt) {
+function saveProfilePopup(evt) {
     evt.preventDefault();
     setName.textContent = inputName.value;
     setDesc.textContent = inputDesc.value;
     setPopup.classList.remove('popup_opened');
-});
-
-for (let i = 0; i < btnLike.length; i++) {
-    btnLike[i].addEventListener('click', function () {
-        if (btnLike[i].className === 'place-card__like-btn') {
-            btnLike[i].classList.replace('place-card__like-btn', 'place-card__like-btn_active')
-        } else {
-            btnLike[i].classList.replace('place-card__like-btn_active', 'place-card__like-btn')
-        }
-    });
 }
+
+btnProfileEdit.addEventListener('click', showProfilePopup);
+btnExitProfile.addEventListener('click', showProfilePopup);
+formProfile.addEventListener('submit', saveProfilePopup);
